242e93e50d08056e26e177fb889eed816a247ee5
jcheng14 <56560114+jcheng14@users.noreply.github.com>
2023-07-02 23:01:17 -0700

    Add Python file

---
+++ ml_model.py
@@ -0,0 +1,15 @@
+from sklearn.linear_model import LinearRegression
+
+#Create sample training data
+X = [[1], [2], [3], [4], [5]]
+y = [2, 4, 6, 8, 10]
+
+#Train the model
+model = LinearRegression()
+model.fit(X,y)
+
+#Make predictions
+X_test = [[6], [7]]
+y_pred = model.predict(X_test)
+
+print(y_pred)
